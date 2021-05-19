ddf9146124dfefcc2b13a0d1d8e931da78c1ec55
jonathand03 <54506186+jonathand03@users.noreply.github.com>
2021-05-19 00:50:56 -0300

    Novas alterações na leitura do can
    Adicionada função não bloqueante para o socket do can_frame, onde é possível ler um dado do can sem travar a aplicação rodando no linux embarcado

--- get-frame-log.c
+++ get-frame-log.c
@@ -16,35 +16,41 @@ don't hold back to tell me :)
 #include <sys/socket.h>
 #include <linux/can.h>
 #include <linux/can/raw.h>
-/*--------------------------------------*/
 
+/*********** FLAGS *********************/
+typedef struct can_frame can_frame_t;
+uint8_t TIME_TO_READ = 0;
+uint8_t ERROR_FILE_DESCRIPTOR_SET = 0;
+uint8_t WAITING_CAN_FRAME = 0;
+
+/*********** FUNCTIONS ******************/
 /*!
  * @brief Creates a virtual socket with the name of 'virtual_socket'
  * @return Returns 0 if the socket was created successfully, otherwise, returns 1
  */
 uint8_t create_virtual_socket(void)
 {
-    int check_return ;
+    int check_return;
     char *VIRTUAL_SOCKET_NAME = "virtual_socket";
     const uint8_t LINUX_COMMAND_MAX_LEN = 100;
     char linux_command[LINUX_COMMAND_MAX_LEN];
 
-	// Commands to create a virtual socket on kernel linux	
-	strcpy(linux_command, "sudo modprobe vcan");
-	system(linux_command);
-	sprintf(linux_command,"sudo ip link add dev %s type vcan", VIRTUAL_SOCKET_NAME);
-	system(linux_command);
-	sprintf(linux_command,"sudo ip link set up %s", VIRTUAL_SOCKET_NAME);
-	check_return = system(linux_command);
+    // Commands to create a virtual socket on kernel linux
+    strcpy(linux_command, "sudo modprobe vcan");
+    check_return = system(linux_command);
+    sprintf(linux_command, "sudo ip link add dev %s type vcan", VIRTUAL_SOCKET_NAME);
+    check_return = system(linux_command);
+    sprintf(linux_command, "sudo ip link set up %s", VIRTUAL_SOCKET_NAME);
+    check_return = system(linux_command);
 
-	if(check_return == 0)
+    if (check_return == 0)
     {
-        printf(" Created virtual module, with name of %s \n", VIRTUAL_SOCKET_NAME);
+        printf("Created virtual module, with name of %s", VIRTUAL_SOCKET_NAME);
         return 0;
     }
     else
-    {   
-        printf(" \n Error creating virtual module \n");
+    {
+        printf("Error creating virtual module");
         return 1;
     }
 }
@@ -55,11 +61,11 @@ uint8_t create_virtual_socket(void)
  */
 int open_virtual_socket(void)
 {
-    char *VIRTUAL_SOCKET_NAME = "virtual_socket";                                
+    char *VIRTUAL_SOCKET_NAME = "virtual_socket";
     // Opening and binding to a can socket
-    int s;   
-    // Verify if socket exist                                       
-    if((s = socket(PF_CAN,SOCK_RAW,CAN_RAW)) < 0)
+    int s;
+    // Verify if socket exist
+    if ((s = socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0)
     {
         perror("Socket");
         return 1;
@@ -67,7 +73,7 @@ int open_virtual_socket(void)
     // Create a data frame struct to store data of 'cansend'
     struct ifreq ifr;
     // Copy the name to frame struct
-    strcpy(ifr.ifr_name, VIRTUAL_SOCKET_NAME); 
+    strcpy(ifr.ifr_name, VIRTUAL_SOCKET_NAME);
     // control of socket
     ioctl(s, SIOCGIFINDEX, &ifr);
 
@@ -75,7 +81,7 @@ int open_virtual_socket(void)
     memset(&addr, 0, sizeof(addr));
     addr.can_family = AF_CAN;
     addr.can_ifindex = ifr.ifr_ifindex;
-    if (bind(s, (struct sockaddr *)&addr, sizeof(addr)) < 0) 
+    if (bind(s, (struct sockaddr *)&addr, sizeof(addr)) < 0)
     {
         perror("Bind");
         return 1;
@@ -83,7 +89,113 @@ int open_virtual_socket(void)
     else
     {
         return s;
-    }                     
+    }
+}
+
+can_frame_t get_frame_data(int sock, int reading_watcher)
+{
+	// Read a frame
+	//int nbytes;
+	can_frame_t frame; 
+	if(reading_watcher == -1)
+	{
+		frame.can_id = -1;
+		return frame;
+	}
+	else if(reading_watcher == 0)
+	{
+		frame.can_id = -2;
+		return frame;
+	}
+	else
+	{
+		read(sock, &frame, sizeof(can_frame_t));
+		return frame;
+	}
+}
+ 
+ /*!
+ * @brief A debug to check the data stored in the can_frame
+ * @param[in] frame The can frame
+ * @return Returns 0 if it shows the data successfully, returns 1 otherwise.
+ */
+ uint8_t debugCAN(can_frame_t frame)
+{ 
+	int i;
+	if(frame.can_id == -1)
+	{
+		printf("ERRO\n");
+		return 1;
+	}
+	else if(frame.can_id == -2)
+	{
+		printf("Waiting Data \n");
+		return 2;
+	}
+	else
+	{
+		printf("0x%03X [%d] ",frame.can_id, frame.can_dlc);
+		for (i = 0; i < frame.can_dlc; i++)
+			printf("%02X ",frame.data[i]);
+		printf("\r\n");
+		return 0;
+	}
+}
+
+/*!
+ * @brief Close the virtual socket from the linux device list.
+ * @return The status of remove socket. 0 if removed with success, otherwise, returns 1 if not succefull.
+ */
+uint8_t close_socket(void)
+{
+	// To check the return value of system function
+    int check_return;
+    // Var to store the command in linux terminal.
+    char command[100];
+	// send a command string to 'command' var
+    strcpy(command, "sudo modprobe -r vcan");
+    // set system returns on check_return
+    check_return = system(command);
+
+    if (check_return == 0)
+    {
+        printf(" Virtual device removed\n ");
+        return 0;
+    }
+    else
+    {
+        printf(" Error removing virtual device \n ");
+        return 1;
+    }
+}
+
+/*!
+ * @brief Verifica se há dados no socket baseado no tempo definido em 50 milisegundos.
+ * @param[in] socket é o socket de onde será pegue os dados.
+ * @return No caso de sucesso, retorna os dados gerados no socket, caso erro, retorna o codigo de erro,
+ * -1 para erro na leitura, -2 para timeout.
+ */
+int timer_routine_service_int(int socket)
+{
+    const int ERROR = -1;
+    const int WAITING_DATA = 0;
+    const int READY_TO_READ = 1;
+    // Flag de tratamento do timeout
+    int socket_watcher;
+    
+    struct pollfd event_test[1];
+    event_test[0].fd = socket;
+    event_test[0].events = POLLIN;
+    event_test[0].revents = POLLIN;
+    
+    socket_watcher = poll(event_test,1,1000);
+    
+    if(socket_watcher == -1)
+        return ERROR;
+    else if(socket_watcher == 0)
+        return WAITING_DATA;
+    else
+        return READY_TO_READ;
 }
 
 /*!
@@ -92,7 +204,7 @@ int open_virtual_socket(void)
  * @return Returns the can_frame structure with the stored data,otherwise, the value of -1 
  * will return in can.id, reporting a reading error.
  */
-can_frame get_frame_data(int s)
+can_frame_t get_frame_data(int s)
 {
     // Read a frame
     int nbytes;
@@ -101,9 +213,9 @@ can_frame get_frame_data(int s)
 
     if (nbytes < 0) 
     {
-		frame.can_id = -1;
+	frame.can_id = -1;
     }
-	return frame;
+    return frame;
 }
 
 /*!
@@ -129,32 +241,6 @@ uint8_t debugCAN(can_frame frame)
 		printf("\r\n");
 		return 0;
 	}
-}
-
-/*!
- * @brief Get the pgn with frequency filter from a can frame
- * @param[in] frame The can frame
- * @param[in] pgn The pgn
- * @return The pgn after a certain time
- */
-uint8_t close_socket(void)
-{
-    int check_return;
-	char command[100];
-
-	strcpy(command, "sudo modprobe -r vcan");
-	check_return = system(command);
-
-    if(check_return == 0)
-    {
-        printf(" Virtual device removed ");
-        return 0;
-    }
-    else
-    {
-        printf(" Error removing virtual device ");
-        return 1;
-    }
 }
 
 /*!
