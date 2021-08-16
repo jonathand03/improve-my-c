1c374dc639c05e2115291dd60fec0bfe41de6e1e
Jonathan Drumond <54506186+jonathand03@users.noreply.github.com>
2021-08-15 22:08:41 -0300

    Alterações para conformidade com as funções no arquivo cpp
    Agora é possível adicionar módulos pelo endereço, ativar/desativar módulos, fazer a leitura apenas do giroscopio.

--- STEVE_v1.0/giroscopio/dev/giroscopio/lib/Giroscopio/giroscopio.h
+++ STEVE_v1.0/giroscopio/dev/giroscopio/lib/Giroscopio/giroscopio.h
@@ -13,52 +13,28 @@
 #ifndef GIROSCOPIO_H
 #define GIROSCOPIO_H
 
-#include <Adafruit_Sensor.h>
+#define ENDERECO_MPU_6050
+#ifdef ENDERECO_MPU_6050
+
+#define ENDERECO_0X68 0x68
+#define ENDERECO_0X69 0x69
+
+#endif
+
+
+#include <Adafruit_MPU6050.h>
+
+extern const int endereco_mpu_modulo_1;
+extern const int endereco_mpu_modulo_2;
 
 /* 
 *   Área de exportação de funções 
 */
-int inicializa_mpu6050(void);
-sensors_event_t *leitura_sensor_mpu(sensors_event_t *vetor_dados_mpu);
-int mostra_dados_giroscopio(sensors_event_t *leitura_sensor_mpu);
-int mostra_dados_acelerometro(sensors_event_t *leitura_sensor_mpu);
-int formatador_serial_print_giroscopio(float *vetor_eixo_giroscopio);
-int formatador_serial_print_acelerometro(float *vetor_eixo_acelerometro);
-sensors_event_t *desativa_status_mpu6050(sensors_event_t *vetor_dados_mpu);
-sensors_event_t *ativa_status_mpu6050(sensors_event_t *vetor_dados_mpu);
+int inicializa_sensor_mpu(int endereco_sensor_mpu, Adafruit_MPU6050 &Sensor);
+sensors_event_t leitura_sensor_mpu(Adafruit_MPU6050 &Sensor);
+void mostra_dados_giroscopio(sensors_event_t leitura_sensor_mpu);
+bool modo_operacional_sensor_mpu(Adafruit_MPU6050 &Sensor, bool status);
 
-/*! \enum eixos_sensor_mpu
-*   \brief Eixos do sensor MPU6050
-*  
-*    *********
-*/
-enum eixos_sensor_mpu
-{
-    x,
-    y,
-    z
-};
-
-/*! \enum dados_mpu
-*   \brief Objetos da estrutura do MPU
-*  
-*    *********
-*/
-enum dados_mpu
-{
-    aceleracao,
-    giroscopio
-};
-
-/*! \enum modo_operacional_sensor
-*   \brief Atividade do sensor
-*  
-*    *********
-*/
-enum modo_operacional_sensor
-{
-    ativado,
-    desativado
-};
-
+extern bool ativado;
+extern bool desativado;
 #endif
