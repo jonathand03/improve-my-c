eae1d6f87d1cb4f8f5dbe1ce6c4b1388e74984c7
Jonathan Drumond <54506186+jonathand03@users.noreply.github.com>
2021-08-16 23:58:43 -0300

    Melhorias para o padrão TDD para embarcados
    Agora é possível executar os testes unitários das funções

--- STEVE_v1.0/giroscopio/dev/giroscopio/lib/Giroscopio/giroscopio.cpp
+++ STEVE_v1.0/giroscopio/dev/giroscopio/lib/Giroscopio/giroscopio.cpp
@@ -2,12 +2,13 @@
 #include "giroscopio.h"
 #include "steve_debug.h"
 
+
+
+
 extern const int endereco_mpu_modulo_1 = ENDERECO_0X68;
 extern const int endereco_mpu_modulo_2 = ENDERECO_0X69;
-extern bool ativado = true;
-extern bool desativado= false;
 
-/*! \fn int inicializa_mpu6050(void)
+/*! @fn int inicializa_mpu6050(void)
  * @brief Inicializa o módulo MPU6050, definindo a calibração do 
  * sensores acelerômetro e giroscópio
  * @return Retorna  SUCESSO , caso o sensor seja inicializado com sucesso. Retorna  SINALIZADOR_ERRO , caso falhe.
@@ -26,7 +27,7 @@ int inicializa_sensor_mpu(int endereco_sensor_mpu, Adafruit_MPU6050 &Sensor)
         return SUCESSO;
 }
 
-/*! \fn sensors_event_t *leitura_sensor_mpu(sensors_event_t *vetor_dados_mpu)
+/*! @fn sensors_event_t *leitura_sensor_mpu(sensors_event_t *vetor_dados_mpu)
  * @brief   Faz a leitura dos sensores [ Giroscópio] e [Acelerômetro] do MPU6050
  * @param[in] Sensor é o objeto do tipo Adafruit_MPU6050 onde é armazenado
  * a estrutura do Sensor mpu_6050
@@ -34,7 +35,7 @@ int inicializa_sensor_mpu(int endereco_sensor_mpu, Adafruit_MPU6050 &Sensor)
 */
 sensors_event_t leitura_sensor_mpu(Adafruit_MPU6050 &Sensor)
 {
-
+        bool status;
         sensors_event_t leitura_acelerometro; //!< Variável que irá guardar a leitura do acelerometro
         sensors_event_t leitura_giroscopio;     //!< Variável que irá guardar a leitura do giroscopio
         sensors_event_t leitura_temperatura;  //!< Variável que irá guardar a leitura da temperatura
@@ -44,34 +45,46 @@ sensors_event_t leitura_sensor_mpu(Adafruit_MPU6050 &Sensor)
         *
         *   ******
         */
-        Sensor.getEvent(&leitura_acelerometro, &leitura_giroscopio, &leitura_temperatura);
-
-        return leitura_giroscopio; //<! Retornando o vetor com os dados lidos
+        status = Sensor.getEvent(&leitura_acelerometro, &leitura_giroscopio, &leitura_temperatura);
+        if(status == false) //<! Caso não seja possível fazer a leitura
+        {
+                debug("Não foi possível ler os dados do sensor");
+                leitura_giroscopio.gyro.status = -1;
+                return  leitura_giroscopio;
+        }
+        else
+        {
+                leitura_giroscopio.gyro.status = 0;
+                return leitura_giroscopio; //<! Retorna a estrutura com os dados do giroscopio
+        }
 }
 
 
-/*! \fn void mostra_dados_giroscopio(sensors_event_t leitura_sensor_mpu)
+/*! @fn void mostra_dados_giroscopio(sensors_event_t leitura_sensor_mpu)
  * @brief Mostra na Serial os dados do MPU6050 referente ao [Giroscopio]
  * @param[in] leitura_sensor_mpu é a função de captura de dados do MPU6050 
  * @return Retorna  SUCESSO , caso os dados sejam mostrados. 
  * Retorna SINALIZADOR_ERRO , caso falhe na mostragem de dados.
 */
-void mostra_dados_giroscopio(sensors_event_t leitura_sensor_mpu)
+int mostra_dados_giroscopio(sensors_event_t leitura_sensor_mpu)
 {
+        if(leitura_sensor_mpu.gyro.status == SINALIZADOR_ERRO)
+                return SINALIZADOR_ERRO;
+        
         Serial.println();
         Serial.print("Rotacao x: ");Serial.print(leitura_sensor_mpu.gyro.x);
         Serial.print(" Rotacao y: ");Serial.print(leitura_sensor_mpu.gyro.y);
         Serial.print(" Rotacao z: ");Serial.print(leitura_sensor_mpu.gyro.z);
         Serial.println();
+        return SUCESSO;
 }
 
-/*!
-        Altera o modo operacional do sensor repassado por parametro
-        @param[in] Sensor é sensor que terá seu modo operacional alterado
-        @param[in] status é o modo operacional que o sensor irá ficar
-        @return Retorna Sucesso se o sensor conseguir alterar o modo operacional, Retorna Sinalizador
-        erro caso contrario
-*/
+/*! @fn modo_operacional_sensor_mpu(Adafruit_MPU6050 &Sensor, bool status)
+ * @brief Altera o modo operacional do sensor repassado por parametro
+ * @param[in] Sensor é sensor que terá seu modo operacional alterado
+ * @param[in] status é o modo operacional que o sensor irá ficar
+ * @return Retorna SUCESSO caso o sensor consega alterar o modo operacional, Retorna SINALIZADOR_ERRO, caso contrario.
+ */
 bool modo_operacional_sensor_mpu(Adafruit_MPU6050 &Sensor, bool status)
 {
         return Sensor.enableSleep(status);
