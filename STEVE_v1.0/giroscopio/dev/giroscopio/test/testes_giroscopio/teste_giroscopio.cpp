84b34edd68de774a9f2ef0d06905436baa2255d4
Jonathan Drumond <54506186+jonathand03@users.noreply.github.com>
2021-08-17 00:00:17 -0300

    Criação dos testes unitarios
    Os novos testes unitáriso foram criados com sucesso

--- STEVE_v1.0/giroscopio/dev/giroscopio/test/testes_giroscopio/teste_giroscopio.cpp
+++ STEVE_v1.0/giroscopio/dev/giroscopio/test/testes_giroscopio/teste_giroscopio.cpp
@@ -4,124 +4,73 @@
 
 #ifdef UNIT_TEST
 
+Adafruit_MPU6050 Mpu1;
+Adafruit_MPU6050 Mpu2;
+
 /*!
 *       Casos de Sucesso 
 */
-void test_inicializa_mpu6050(void)
+void test_inicializa_sensor_mpu(void)
 {
         int verifica_retorno_inicializa_mpu6050;
         int valor_esperado_inicializa_mpu6050 = SUCESSO;
 
-        verifica_retorno_inicializa_mpu6050 = inicializa_mpu6050();
+        verifica_retorno_inicializa_mpu6050 = inicializa_sensor_mpu(endereco_mpu_modulo_1,Mpu1);
         TEST_ASSERT_EQUAL(valor_esperado_inicializa_mpu6050, verifica_retorno_inicializa_mpu6050);
 
 }
 
-void test_leitura_sensor_mpu_sucesso(void)
+void test_leitura_sensor_mpu(void)
 {
-        sensors_event_t dados_mpu[2];
-        sensors_event_t *verifica_retorno_leitura_sensor = NULL;
-        int valor_esperado_status_sensor = ativado;
+        sensors_event_t verifica_retorno_leitura_sensor_mpu;
 
-        verifica_retorno_leitura_sensor = leitura_sensor_mpu(dados_mpu);
-        TEST_ASSERT_EQUAL(valor_esperado_status_sensor,dados_mpu[giroscopio].gyro.status);
-        TEST_ASSERT(verifica_retorno_leitura_sensor != NULL);
+        verifica_retorno_leitura_sensor_mpu = leitura_sensor_mpu(Mpu1);
+        TEST_ASSERT(verifica_retorno_leitura_sensor_mpu.gyro.status == SUCESSO);
 }
 
-void test_mostra_dados_giroscopio_sucesso(void)
+void test_mostra_dados_giroscopio(void)
 {
-        sensors_event_t dados_mpu[2];
         int verifica_retorno_mostra_dados_giroscopio;
-        int valor_esperado_mostra_dados_giroscopio = SUCESSO;
-        
-        verifica_retorno_mostra_dados_giroscopio = mostra_dados_giroscopio(leitura_sensor_mpu(dados_mpu));
-        TEST_ASSERT_EQUAL(valor_esperado_mostra_dados_giroscopio,verifica_retorno_mostra_dados_giroscopio);
-}
+        int valor_esperado_retorno_mostra_dados_giroscopio = SUCESSO;
 
-void test_mostra_dados_acelerometro_sucesso(void)
-{
-        sensors_event_t dados_mpu[2];
-        int verifica_retorno_mostra_dados_acelerometro;
-        int valor_esperado_mostra_dados_acelerometro = SUCESSO;
-        
-        verifica_retorno_mostra_dados_acelerometro = mostra_dados_acelerometro(leitura_sensor_mpu(dados_mpu));
-        TEST_ASSERT_EQUAL(valor_esperado_mostra_dados_acelerometro,verifica_retorno_mostra_dados_acelerometro);
-}
-
-void test_formatador_serial_print_giroscopio_sucesso(void)
-{
-        float vetor_artificial_eixos[3] = {1.32, 7.32, -5.3};
-        int verifica_retorno_formatador_serial_print_giroscopio;
-        int valor_esperado_formatador_serial_print_giroscopio = SUCESSO;
-
-        verifica_retorno_formatador_serial_print_giroscopio = formatador_serial_print_giroscopio(vetor_artificial_eixos);
-        TEST_ASSERT_EQUAL(valor_esperado_formatador_serial_print_giroscopio,verifica_retorno_formatador_serial_print_giroscopio);
-}
-
-void test_formatador_serial_print_acelerometro_sucesso(void)
-{
-        float vetor_artificial_eixos[3] = {1, 27.06, -5.3};
-        int verifica_retorno_formatador_serial_print_acelerometro;
-        int valor_esperado_formatador_serial_print_acelerometro = SUCESSO;
-        
-        verifica_retorno_formatador_serial_print_acelerometro = formatador_serial_print_acelerometro(vetor_artificial_eixos);
-        TEST_ASSERT_EQUAL(valor_esperado_formatador_serial_print_acelerometro,verifica_retorno_formatador_serial_print_acelerometro);
+        verifica_retorno_mostra_dados_giroscopio = mostra_dados_giroscopio(leitura_sensor_mpu(Mpu1));
+        TEST_ASSERT_EQUAL(valor_esperado_retorno_mostra_dados_giroscopio,verifica_retorno_mostra_dados_giroscopio);
 }
 
 /*!
 *      Casos de Falha
 */
+void test_inicializa_sensor_mpu_falha(void)
+{
 
+        int verifica_retorno_inicializa_mpu6050;
+        int valor_esperado_inicializa_mpu6050 = SINALIZADOR_ERRO;
+
+        verifica_retorno_inicializa_mpu6050 = inicializa_sensor_mpu(0x72,Mpu1);
+        TEST_ASSERT_EQUAL(valor_esperado_inicializa_mpu6050, verifica_retorno_inicializa_mpu6050);
+}
 void test_leitura_sensor_mpu_falha(void)
 {
+        sensors_event_t verifica_retorno_leitura_sensor_mpu;
 
-}
-
-void test_mostra_dados_giroscopio_falha(void)
-{
-
-}
-
-void test_mostra_dados_acelerometro_falha(void)
-{
-
-}
-
-void formatador_serial_print_giroscopio_falha(void)
-{
-
-}
-
-void formatador_serial_print_acelerometro_falha(void)
-{
+        verifica_retorno_leitura_sensor_mpu = leitura_sensor_mpu(Mpu1);
+        TEST_ASSERT(verifica_retorno_leitura_sensor_mpu.gyro.status == SINALIZADOR_ERRO);
 }
 
 void setup()
 {
         //inicializa_mpu6050();
         UNITY_BEGIN();
-        RUN_TEST(test_inicializa_mpu6050);
+        RUN_TEST(test_inicializa_sensor_mpu);
+        RUN_TEST(test_inicializa_sensor_mpu_falha);
+        RUN_TEST(test_leitura_sensor_mpu_falha);
+        UNITY_END();
       
 }
 
-int i = 0;
-int parada = 10;
 
 void loop()
 {
-        if(i < parada)
-        {
-                RUN_TEST(test_leitura_sensor_mpu_sucesso);
-                delay(100);
-                RUN_TEST(test_mostra_dados_giroscopio_sucesso);
-                delay(100);
-                RUN_TEST(test_formatador_serial_print_giroscopio_sucesso);
-                i++;
-                delay(6000); 
-        }
-        else if(i == parada)
-        {
-                UNITY_END();
-        }
+      
 }
 #endif
