b9240c07b0b9614e5b2f0365e418951e22962d19
Jonathan Drumond <54506186+jonathand03@users.noreply.github.com>
2021-08-18 10:19:02 -0300

    Melhoria nos testes

--- STEVE_v1.0/giroscopio/dev/giroscopio/test/testes_giroscopio/teste_giroscopio.cpp
+++ STEVE_v1.0/giroscopio/dev/giroscopio/test/testes_giroscopio/teste_giroscopio.cpp
@@ -2,25 +2,28 @@
 #include <steve_debug.h>
 #include <unity.h>
 
-#ifdef UNIT_TEST
-
 Adafruit_MPU6050 Mpu1;
 Adafruit_MPU6050 Mpu2;
+Adafruit_MPU6050 Mpu3;
+
+
+#ifdef UNIT_TEST
+
 
 /*!
 *       Casos de Sucesso 
 */
-void test_inicializa_sensor_mpu(void)
+void test_inicializa_sensor_mpu_sucesso(void)
 {
-        int verifica_retorno_inicializa_mpu6050;
-        int valor_esperado_inicializa_mpu6050 = SUCESSO;
+        int verifica_retorno_test_inicializa_sensor_mpu;
+        int valor_esperado_test_inicializa_sensor_mpu = SUCESSO;
 
-        verifica_retorno_inicializa_mpu6050 = inicializa_sensor_mpu(endereco_mpu_modulo_1,Mpu1);
-        TEST_ASSERT_EQUAL(valor_esperado_inicializa_mpu6050, verifica_retorno_inicializa_mpu6050);
+        verifica_retorno_test_inicializa_sensor_mpu = inicializa_sensor_mpu(endereco_mpu_modulo_1,Mpu1);
+        TEST_ASSERT_EQUAL(valor_esperado_test_inicializa_sensor_mpu, verifica_retorno_test_inicializa_sensor_mpu);
 
 }
 
-void test_leitura_sensor_mpu(void)
+void test_leitura_sensor_mpu_sucesso(void)
 {
         sensors_event_t verifica_retorno_leitura_sensor_mpu;
 
@@ -28,14 +31,6 @@ void test_leitura_sensor_mpu(void)
         TEST_ASSERT(verifica_retorno_leitura_sensor_mpu.gyro.status == SUCESSO);
 }
 
-void test_mostra_dados_giroscopio(void)
-{
-        int verifica_retorno_mostra_dados_giroscopio;
-        int valor_esperado_retorno_mostra_dados_giroscopio = SUCESSO;
-
-        verifica_retorno_mostra_dados_giroscopio = mostra_dados_giroscopio(leitura_sensor_mpu(Mpu1));
-        TEST_ASSERT_EQUAL(valor_esperado_retorno_mostra_dados_giroscopio,verifica_retorno_mostra_dados_giroscopio);
-}
 
 /*!
 *      Casos de Falha
@@ -43,17 +38,18 @@ void test_mostra_dados_giroscopio(void)
 void test_inicializa_sensor_mpu_falha(void)
 {
 
-        int verifica_retorno_inicializa_mpu6050;
-        int valor_esperado_inicializa_mpu6050 = SINALIZADOR_ERRO;
+        int verifica_retorno_test_inicializa_sensor_mpu;
+        int valor_esperado_test_inicializa_sensor_mpu = SINALIZADOR_ERRO;
 
-        verifica_retorno_inicializa_mpu6050 = inicializa_sensor_mpu(0x72,Mpu1);
-        TEST_ASSERT_EQUAL(valor_esperado_inicializa_mpu6050, verifica_retorno_inicializa_mpu6050);
+        verifica_retorno_test_inicializa_sensor_mpu = inicializa_sensor_mpu(0x72,Mpu1);
+        TEST_ASSERT_EQUAL(valor_esperado_test_inicializa_sensor_mpu, verifica_retorno_test_inicializa_sensor_mpu);
 }
 void test_leitura_sensor_mpu_falha(void)
 {
         sensors_event_t verifica_retorno_leitura_sensor_mpu;
 
-        verifica_retorno_leitura_sensor_mpu = leitura_sensor_mpu(Mpu1);
+        verifica_retorno_leitura_sensor_mpu = leitura_sensor_mpu(Mpu3);
+        Serial.println(verifica_retorno_leitura_sensor_mpu.gyro.status);
         TEST_ASSERT(verifica_retorno_leitura_sensor_mpu.gyro.status == SINALIZADOR_ERRO);
 }
 
@@ -61,8 +57,9 @@ void setup()
 {
         //inicializa_mpu6050();
         UNITY_BEGIN();
-        RUN_TEST(test_inicializa_sensor_mpu);
+        RUN_TEST(test_inicializa_sensor_mpu_sucesso);
         RUN_TEST(test_inicializa_sensor_mpu_falha);
+        RUN_TEST(test_leitura_sensor_mpu_sucesso);
         RUN_TEST(test_leitura_sensor_mpu_falha);
         UNITY_END();
       
