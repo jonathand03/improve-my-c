93afdb79edb6df0d85583d109f9bafdb1baf04d9
jonathand03 <jonathan.drumond@mobs2.com>
2022-02-06 20:05:32 -0300

    melhorias BOTOES

--- estudo_display/lib/Botoes/botoes.cpp
+++ estudo_display/lib/Botoes/botoes.cpp
@@ -1,110 +1,86 @@
-#include "botoes.hpp"
+#include "botoes.h"
 
-enum estado_botao
-{
-  desligado,
-  ligado,
-  continua_desligado,
-  continua_ligado
-};
-enum teclas_menu
-{
-    bt_baixo,
-    bt_cima,
-    bt_enter,
-    bt_standup,
-    bt_panico
-};
-int tecla = 0;
-int estado_botoes_ihm[4] = {desligado,desligado,desligado,desligado};
-int estado_bt_anterior[4] = {desligado,desligado,desligado,desligado};
+
+int QTD_BT = 0;
+extern int estado_botoes_ihm[qtd_bt_ihm] = {desligado, desligado, desligado, desligado};
+extern int estado_bt_anterior[qtd_bt_ihm] = {desligado, desligado, desligado, desligado};
+
 
 void InterrruptFlag(void *args)
 {
-  int bt = (int) args;
-  estado_botoes_ihm[bt] = ligado;
-}
-
-
-estado_botao leitura_botao(teclas_menu tecla)
-{
-
-}
-
-
-/* Inicializando botão sem utilizar interrupção de hardware */
-Button::Button(int pin)
-{
-   pinMode(pin,INPUT);
+    int bt = (int )args;
+    estado_botoes_ihm[bt] = ligado;
 }
 
 /* Inicializando botão utilizando interrupção de hardware */
-Button::Button(int pin, gpio_int_type_t edge, void (*fn)())
+Button::Button(int pin, int edge)
 {
-        ButtonPin = pin;
-        esp_err_t DRIVER_INSTALL_RETURN;
-        esp_err_t INTR_TYPE_RETURN;
-        esp_err_t ENABLE_INTR_RETURN;
-        esp_err_t INTR_ADD_RETURN;
-        esp_err_t SIGNAL_PIN_RETURN;
-
-        DRIVER_INSTALL_RETURN = gpio_install_isr_service(0);
-        INTR_TYPE_RETURN      = gpio_set_intr_type((gpio_num_t)pin, edge);
-        ENABLE_INTR_RETURN    = gpio_intr_enable((gpio_num_t)pin);
-        INTR_ADD_RETURN       = gpio_isr_handler_add((gpio_num_t)pin, (gpio_isr_t)InterrruptFlag, (void *) 1); // TODO
-        SIGNAL_PIN_RETURN     = gpio_set_direction((gpio_num_t)pin, GPIO_MODE_INPUT);
-
-        if(DRIVER_INSTALL_RETURN != ESP_OK) {Serial.println("Erro ao instalar o Driver de interrupcao");}
-        if(INTR_TYPE_RETURN != ESP_OK)      {Serial.println("Erro ao inicializar o tipo de interrupcao no pino");}
-        if(ENABLE_INTR_RETURN != ESP_OK)    {Serial.println("Erro ao ativar a interrupção no pino");}
-        if(INTR_ADD_RETURN != ESP_OK)       {Serial.println("Erro ao adicionar a interrupção no pino");}
-        if(SIGNAL_PIN_RETURN != ESP_OK)     {Serial.println("Erro ao adicionar o modo de operação do pino");}
+    if (QTD_BT == 0) // entra aqui primeira vez
+    {
+        attachInterruptArg(pin, InterrruptFlag, (void *)QTD_BT, edge);
+        ButtonStatus.ButtonID  = QTD_BT;
+        Serial.print(QTD_BT);
+    }
+    else
+    {
+        attachInterruptArg(pin, InterrruptFlag, (void *)QTD_BT+1, edge);
+        ButtonStatus.ButtonID = QTD_BT+1;
+    }
+    QTD_BT++;
+    ButtonStatus.ButtonEdge = edge;
+    ButtonStatus.ButtonPin = pin;
+    Serial.print( ButtonStatus.ButtonPin);
+    Serial.print(ButtonStatus.ButtonEdge);
+    
 }
-
-
 
 uint8_t Button::ResumeButton(void)
 {
-
-    /* TODO
-        VERIFICAR O BOTÃO DA CLASSE E ATUALIZAR O ARGUMENTO DO ADD INTR HANDLER
-    */
-        esp_err_t DRIVER_INSTALL_RETURN;
-        esp_err_t INTR_TYPE_RETURN;
-        esp_err_t ENABLE_INTR_RETURN;
-        esp_err_t INTR_ADD_RETURN;
-        esp_err_t SIGNAL_PIN_RETURN;
-
-        DRIVER_INSTALL_RETURN = gpio_install_isr_service(0);
-        INTR_TYPE_RETURN      = gpio_set_intr_type((gpio_num_t)ButtonPin, GPIO_INTR_HIGH_LEVEL);
-        ENABLE_INTR_RETURN    = gpio_intr_enable((gpio_num_t)ButtonPin);
-        INTR_ADD_RETURN       = gpio_isr_handler_add((gpio_num_t)ButtonPin, (gpio_isr_t)InterrruptFlag, (void *) 1);
-        SIGNAL_PIN_RETURN     = gpio_set_direction((gpio_num_t)ButtonPin, GPIO_MODE_INPUT);
-
-        if(DRIVER_INSTALL_RETURN != ESP_OK) {Serial.println("Erro ao instalar o Driver de interrupcao");}
-        if(INTR_TYPE_RETURN != ESP_OK)      {Serial.println("Erro ao inicializar o tipo de interrupcao no pino");}
-        if(ENABLE_INTR_RETURN != ESP_OK)    {Serial.println("Erro ao ativar a interrupção no pino");}
-        if(INTR_ADD_RETURN != ESP_OK)       {Serial.println("Erro ao adicionar a interrupção no pino");}
-        if(SIGNAL_PIN_RETURN != ESP_OK)     {Serial.println("Erro ao adicionar o modo de operação do pino");}
+   if(ButtonStatus.BtStopped == true)
+   {
+        attachInterruptArg(ButtonStatus.ButtonPin, InterrruptFlag, (void *)ButtonStatus.ButtonID,  ButtonStatus.ButtonEdge);
+        return 0;
+   }
+   else
+   {
+       return 1;
+   }
+   
 }
 
+
+/* Pausa a interrupção do botão */
 uint8_t Button::StopButton(void)
 {
-    gpio_isr_handler_remove((gpio_num_t)ButtonPin);
+    if (QTD_BT <= 0)
+    {
+        Serial.print("Impossivel parar o botão pois não existe nenhum botão cadastrado");
+        return 1;
+    }
+    else
+    {
+        esp_err_t STATUS;
+        STATUS = gpio_isr_handler_remove((gpio_num_t)ButtonStatus.ButtonPin);
+        if (STATUS == ESP_OK)
+        {
+            ButtonStatus.BtStopped = true;
+            return 0;
+        }
+        else
+        {
+            Serial.print("Erro ao pausar o botão");
+            return 1;
+        }
+    }
 }
 
-
-
-/*
-    TODO: FAZER UMA VARIAVEL QUE SALVA O ESTADO DO BOTÃO NA INTERRUPÇÃO PARA CADA PINO ADICIONADO [TECLAS]
-*/
 estado_botao Button::ReadButton(void)
 {
     estado_botao status_atual_verificao = desligado;
 
-    if (estado_botoes_ihm[tecla] == ligado)
+    if (estado_botoes_ihm[ButtonStatus.ButtonID] == ligado)
     {
-        if (estado_bt_anterior[tecla] == desligado)
+        if (estado_bt_anterior[ButtonStatus.ButtonID] == desligado)
 
             status_atual_verificao = ligado; // retorna ligado
         else
@@ -112,13 +88,12 @@ estado_botao Button::ReadButton(void)
     }
     else
     {
-        if (estado_bt_anterior[tecla] == desligado)
+        if (estado_bt_anterior[ButtonStatus.ButtonID] == desligado)
             status_atual_verificao = continua_desligado; // retorna mantendo desligado
         else
             status_atual_verificao = desligado; // retorna desligando
     }
-    estado_bt_anterior[tecla] = estado_botoes_ihm[tecla];
-    estado_botoes_ihm[tecla] = desligado;
+    estado_bt_anterior[ButtonStatus.ButtonID] = estado_botoes_ihm[ButtonStatus.ButtonID];
+    estado_botoes_ihm[ButtonStatus.ButtonID] = desligado;
     return status_atual_verificao;
 }
-
--- estudo_display/src/main.cpp
+++ estudo_display/src/main.cpp
@@ -1,104 +1,26 @@
 #include <Arduino.h>
 #include "TFT_eSPI.h"
 #include <driver/gpio.h>
+#include <driver/pcnt.h>
+#include "botoes.h"
+#include <rom/gpio.h>
 
-TFT_eSPI Display = TFT_eSPI();
-
-
-uint8_t BUTTON_SIZE = 0;
-
-enum estado_botao
-{
-  desligado,
-  ligado,
-  continua_desligado,
-  continua_ligado
-};
-enum teclas_menu
-{
-    bt_baixo,
-    bt_cima,
-    bt_enter,
-    bt_standup,
-    bt_panico
-};
-
-int estado_botoes_ihm[4] = {desligado,desligado,desligado,desligado};
-int estado_bt_anterior[4] = {desligado,desligado,desligado,desligado};
-
-void InterrruptFlag(void *args)
-{
-  int bt = (int) args;
-  estado_botoes_ihm[bt] = ligado;
-}
-
-
-estado_botao leitura_botao(teclas_menu tecla)
-{
-    estado_botao status_atual_verificao = desligado;
-
-    if (estado_botoes_ihm[tecla] == ligado)
-    {
-        if (estado_bt_anterior[tecla] == desligado)
-
-            status_atual_verificao = ligado; // retorna ligado
-        else
-            status_atual_verificao = continua_ligado; // retorna mantendo ligado
-    }
-    else
-    {
-        if (estado_bt_anterior[tecla] == desligado)
-            status_atual_verificao = continua_desligado; // retorna mantendo desligado
-        else
-            status_atual_verificao = desligado; // retorna desligando
-    }
-    estado_bt_anterior[tecla] = estado_botoes_ihm[tecla];
-    estado_botoes_ihm[tecla] = desligado;
-    return status_atual_verificao;
-}
-
-
-
-int inicializa(int pin ,gpio_int_type_t edge)
-{
-        esp_err_t DRIVER_INSTALL_RETURN;
-        esp_err_t INTR_TYPE_RETURN;
-        esp_err_t ENABLE_INTR_RETURN;
-        esp_err_t INTR_ADD_RETURN;
-        esp_err_t SIGNAL_PIN_RETURN;
-
-        DRIVER_INSTALL_RETURN = gpio_install_isr_service(0);
-        INTR_TYPE_RETURN      = gpio_set_intr_type((gpio_num_t)pin, edge);
-        ENABLE_INTR_RETURN    = gpio_intr_enable((gpio_num_t)pin);
-        INTR_ADD_RETURN       = gpio_isr_handler_add((gpio_num_t)pin, (gpio_isr_t)InterrruptFlag, (void *) 1);
-        SIGNAL_PIN_RETURN     = gpio_set_direction((gpio_num_t)pin, GPIO_MODE_INPUT);
-
-        if(DRIVER_INSTALL_RETURN != ESP_OK) {return -1;}
-        if(INTR_TYPE_RETURN != ESP_OK)      {return -1;}
-        if(ENABLE_INTR_RETURN != ESP_OK)    {return -1;}
-        if(INTR_ADD_RETURN != ESP_OK)       {return -1;}
-        if(SIGNAL_PIN_RETURN != ESP_OK)     {return -1;}
-        return 0;
-}
-
-void remove_interrupcao(int pin)
-{
-  gpio_isr_handler_remove((gpio_num_t)pin);
-}
-
-
+//TFT_eSPI Display = TFT_eSPI();
+Button ALERTA(23, RISING);
 void setup()
 {
   Serial.begin(115200);
-  inicializa(23,GPIO_INTR_HIGH_LEVEL);
-  inicializa(23,GPIO_INTR_HIGH_LEVEL);
+ 
 }
 
 void loop()
 {
-  Serial.print(leitura_botao(bt_baixo));
-  Serial.print(leitura_botao(bt_cima));
-  Serial.print(leitura_botao(bt_enter));
-  Serial.println(leitura_botao(bt_standup));
-  delay(500);
+  ALERTA.ReadButton();
+  Serial.print(estado_botoes_ihm[0]);
+  Serial.print(estado_botoes_ihm[1]);
+  Serial.print(estado_botoes_ihm[2]);
+  Serial.print(estado_botoes_ihm[3]);
+  Serial.println(estado_botoes_ihm[4]);
+ 
+  delay(2000);
 }
