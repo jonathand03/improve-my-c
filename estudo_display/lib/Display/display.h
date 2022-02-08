8cf0115ea851aa4d8e90d19d9e95cceedc9f3821
jonathand03 <jonathan.drumond@mobs2.com>
2022-02-07 23:14:10 -0300

    Funçoes do ihm

--- estudo_display/lib/Display/display.h
+++ estudo_display/lib/Display/display.h
@@ -20,6 +20,29 @@ class IHM : public TFT_eSPI
         void TelaMenuSC(void);
         /*Tela do menu dos microajustes*/
         void TelaMenuAjustes(void);
+        
+
+       
+        /**** Suporte Circulatorio ****/
+        /* Tela de status do suporte ciculatorio */
+        void TelaStatusSC(void);
+        /* Tela de configurações do angulo */
+        void TelaAngulo(void);
+        /* Tela de configurações de ciclo */
+        void TelaCiclo(void);
+        /* Tela de configurações de tempo */
+        void TelaTempo(void);
+
+        /**** Ajustes ****/
+        /* Ajustes Assento */
+        void AjusteVerticalAssento(void);
+        void AjusteHorizontalAssento(void);
+        /* Ajustes Encosto */
+        void AjusteInclinacaoEncosto(void);
+        void AjusteVerticalEncosto(void);
+
+        
+        
 
     private:
 };
