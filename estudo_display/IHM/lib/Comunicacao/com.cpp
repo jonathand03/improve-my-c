e39ae5aff42c9c5880193576f83b1ee56d1a1523
jonathand03 <jonathan.drumond@mobs2.com>
2022-02-17 17:05:53 -0300

    Melhorias COM

--- estudo_display/IHM/lib/Comunicacao/com.cpp
+++ estudo_display/IHM/lib/Comunicacao/com.cpp
@@ -1,1 +1,47 @@
 #include "com.h"
+
+/*! @brief Begin the Serial Communication 
+* @param[in] baud Serial Communication Baud Rate
+* @param[in] rx Serial Reciver Pin
+* @param[in] tx Serial Transciver Pin
+*/
+void SerialCom::SerialInit(int baud, uint8_t rx, uint8_t tx)
+{
+    Serial.begin(SERIAL_BAUD,SERIAL_8N1,this->SerialConfiguration.rxPin,this->SerialConfiguration.txPin);
+    this->SerialConfiguration.IsWorking = true;
+    this->SerialConfiguration.IsStopped = false;
+}
+
+/*! @brief Begin the Serial Communication 
+* @param[in] baud Serial Communication Baud Rate
+* @param[in] rx Serial Reciver Pin
+* @param[in] tx Serial Transciver Pin
+* @param[in] timeout Serial timeout in case of any commnunication error
+*/
+void SerialCom::SerialInit(int baud, uint8_t rx, uint8_t tx, uint32_t timeout)
+{
+    Serial.begin(SERIAL_BAUD,SERIAL_8N1,this->SerialConfiguration.rxPin,this->SerialConfiguration.txPin);
+    Serial.setTimeout(timeout);
+}
+
+uint8_t SerialCom::SerialRead(void)
+{
+    if(Serial.available())
+    {
+        // tratamento de dados
+        return COM_SUCESS;
+    }
+    else
+        return COM_NO_DATA;
+}
+
+uint8_t SerialCom::SerialWrite(uint8_t *package)
+{
+    this->SerialConfiguration.StreamPackage = package; // Intermediate vector saves the data package
+    this->SerialConfiguration.PackageSize = sizeof(this->SerialConfiguration.StreamPackage);
+    if(this->SerialConfiguration.IsWorking == true)
+    {
+        Serial.write(this->SerialConfiguration.StreamPackage,this->SerialConfiguration.PackageSize);
+    }
+    
+}
--- estudo_display/IHM/lib/Comunicacao/com.h
+++ estudo_display/IHM/lib/Comunicacao/com.h
@@ -5,7 +5,7 @@
 #include <Arduino.h>
 
 /* Communcation Pin */
-#define TX_PIN 2
+#define TX_PIN 1
 #define RX_PIN 3
 /* Baud Rate */
 #define SERIAL_BAUD 115200
@@ -13,7 +13,7 @@
 #define COM_SUCESS 0
 #define COM_TIMEOUT 1
 #define COM_ERROR 2
-
+#define COM_NO_DATA 3
 class SerialCom
 {
     private:
@@ -24,11 +24,15 @@ class SerialCom
             uint8_t txPin; //!< Store TX pin 
             uint8_t rxPin;  //!< Store RX pin
             uint8_t *StreamPackage; // Store the transmission package
+            size_t PackageSize;
         }ComStatus; //!< Struct with some data types for the serial communication
     public:
         uint8_t SerialRead(void);
-        uint8_t SerialWrite(void);
-        uint8_t SerialInit(void);
+        uint8_t SerialWrite(uint8_t *package);
+        void SerialInit(int baud, uint8_t rx, uint8_t tx);
+        void SerialInit(int baud, uint8_t rx, uint8_t tx, uint32_t timeout);
+        void SerialStop(void);
+        ComStatus SerialConfiguration;
 };
 
 
