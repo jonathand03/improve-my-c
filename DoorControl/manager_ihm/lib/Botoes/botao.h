0cf9d8fdbd94aa02954bbdfae4d4422d771c4e9e
Jonathan Drumond <54506186+jonathand03@users.noreply.github.com>
2022-07-06 16:01:46 -0300

    Update botao.h

--- DoorControl/manager_ihm/lib/Botoes/botao.h
+++ DoorControl/manager_ihm/lib/Botoes/botao.h
@@ -8,21 +8,32 @@
 #include "display_logica.h"
 #include "com.h"
 #define BUTTON_ON
-
-//------------ TODO: Convert to English ----//
+#define ESP_INTR_FLAG_DEFAULT 0
+// ---- TODO: convert to english -------//
 namespace ButtonSpace
 {
-    enum button_type_t{down,up,enter,standup,button_type_max};
-    enum button_state{on,off,stay_on,stay_off};
+    enum button_type_t{down,up,enter,standup,panic,button_type_max};
+    enum button_state{off,on,stay_on,stay_off, pressed_out};
+
+    
 
     // Flags de tratamento de retorno
-    enum return_button_flags{INSTALL_ERROR = 5,INTR_TYPE_ERROR,INTR_ENABLE_ERROR,GPIO_DIRECTION_ERROR,ISR_ADD_ERROR};
+    enum bt_init_return{INSTALL_ERROR,INTR_TYPE_ERROR,INTR_ENABLE_ERROR,
+                        GPIO_DIRECTION_ERROR,ISR_ADD_ERROR, INIT_SUCESS};
+    enum button_start_return{START_SUCESS, START_ERROR};
+    enum button_pause_return{PAUSE_SUCESS, PAUSE_ERROR};
+    enum button_read_return{READ_SUCESS,READ_ERROR};
     typedef struct
     {
         unsigned long int last_time_read;
         button_type_t button_type;
+        int border_type;
         bool is_paused;
+        int current_state;
+        int last_state;
     }button_info_t;
+
+
     
     //converte valores de Arduino Framework pra Espressif Framework
     gpio_int_type_t conversion_int_type(uint8_t type_to_convert);
@@ -31,14 +42,26 @@ namespace ButtonSpace
     class Button
     {
         public:
+            Button(uint8_t border_activate, button_type_t type);
             uint8_t init(uint8_t border_activate, button_type_t type);
             uint8_t read(void);
             uint8_t start(void);
             uint8_t pause(void);
+            uint8_t process(void (*onPressCallBack)(void),void (*onReleaseCallBack)(void));
             button_info_t button_info;
+
     };
 }
 
 extern gpio_num_t gpio_handle[ButtonSpace::button_type_max];
 double verifica_tensao(int button);
+void teste_press(void);
+void test_out(void);
+
+extern ButtonSpace::Button Up;
+extern ButtonSpace::Button Down;
+extern ButtonSpace::Button Enter;
+extern ButtonSpace::Button Standup;
+extern ButtonSpace::Button Panic;
+
 #endif 
